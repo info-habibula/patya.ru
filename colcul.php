86a0bb3728c3dbb77fc5113a63b05a11ac0f0613
site-razhihan <site.info94@gmail.com>
2017-03-13 16:52:29 +0300

    index конец  php конец

--- colcul.php
+++ colcul.php
@@ -1,6 +1,16 @@
 <?php
-$chislo1=
-$chislo2=
-$znaki=
-$rezultat=
+$chislo1=$_GET['chislo1'];
+$chislo2=$_GET['chislo2'];
+$znaki=$_GET['znaki'];
+$rezultat=$_GET['rezultat'];
+switch ($znaki) {
+    case +:
+       $rezultat= $chislo1 + $chislo2;
+        break;
+    case /:
+        $rezultat= $chislo1 / $chislo2;
+        break;
+    
+}
+
 ?>
